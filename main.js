ef5c4988b6af9ceb4b5465402ebac3444e3b9e5a
mscbr <scibor.maciej@gmail.com>
2018-10-08 19:25:02 +0300

    orientation change of navbar when scrolled down designed

---
+++ main.js
@@ -0,0 +1,34 @@
+//JS engine for mscbr. Development portfolio
+
+function main() {
+
+    //changing navbar orientation due to the scroll position
+    function toSidebar() {
+        var scrollpos = window.scrollY;
+        var navbar = document.getElementById("navbar");
+        
+        function add_class_on_scroll() {
+            navbar.classList.add("sidebar");
+        }
+        
+        function remove_class_on_scroll() {
+            navbar.classList.remove("sidebar");
+        }
+        
+        window.addEventListener('scroll', function(){ 
+            //Here you forgot to update the value
+            scrollpos = window.scrollY;
+        
+            if(scrollpos > 140){
+                add_class_on_scroll();
+            }
+            else {
+                remove_class_on_scroll();
+            }
+            // console.log(scrollpos);
+        });
+    }
+    toSidebar();
+}
+
+$(document).ready(main);
