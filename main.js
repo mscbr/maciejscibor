6eafebe296b922957a323704649cc7b70fc8a59c
mscbr <scibor.maciej@gmail.com>
2018-10-11 09:24:45 +0000

    project tile mask titles animation designed

--- main.js
+++ main.js
@@ -45,13 +45,26 @@ function main() {
     function slideHover() {
         var websitesMask = document.getElementById("websites-mask");
         var websites = document.getElementById("websites");
+        var appsMask = document.getElementById("apps-mask");
+        var apps = document.getElementById("web-apps");
+        var dsMask = document.getElementById("ds-mask");
+        var ds = document.getElementById("data-science");
         // ... more variables
 
         //ading event listeners
         websitesMask.addEventListener("mouseover", downsize);
         websites.addEventListener("mouseover", downsize);
+        appsMask.addEventListener("mouseover", downsizeApps);
+        apps.addEventListener("mouseover", downsizeApps);
+        dsMask.addEventListener("mouseover", downsizeData);
+        ds.addEventListener("mouseover", downsizeData);
+
         websitesMask.addEventListener("mouseout", resize);
         websites.addEventListener("mouseout", resize);
+        appsMask.addEventListener("mouseout", resizeApps);
+        apps.addEventListener("mouseout", resizeApps);
+        dsMask.addEventListener("mouseout", resizeData);
+        ds.addEventListener("mouseout", resizeData);
 
 
         function downsize() {
@@ -59,6 +72,18 @@ function main() {
         }
         function resize() {
             websitesMask.classList.remove("proj-mask-off");
+        }
+        function downsizeApps() {
+            appsMask.classList.add("proj-mask-off");
+        }
+        function resizeApps() {
+            appsMask.classList.remove("proj-mask-off");
+        }
+        function downsizeData() {
+            dsMask.classList.add("proj-mask-off");
+        }
+        function resizeData() {
+            dsMask.classList.remove("proj-mask-off");
         }
 
     }
